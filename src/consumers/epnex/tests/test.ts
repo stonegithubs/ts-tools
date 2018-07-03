cfd97930addc87f2a1400a72fbdef1ff380a50c5
chosan <179817004@qq.com>
2018-07-03 19:39:42 +0800

    ..

--- src/consumers/epnex/tests/test.ts
+++ src/consumers/epnex/tests/test.ts
@@ -1,6 +1,7 @@
 // import Epnex from '../epnex';
-import Koa from '../../../lib/koa';
+import http from 'http';
 import rp from 'request-promise';
+import Koa from '../../../lib/koa';
 
 
 // let ep = new Epnex();
@@ -16,17 +17,21 @@ new Koa([
   }
 ]).listen(8889);
 
-let r = rp.post('https://epnex.io/api/emailValidCode', {
-  form: `{"user_email":"12314@chosan.cn","PvilidCode":"s7b4r"}`,
-  headers: {
-    Host: 'epnex.io',
-    Origin: 'https://epnex.io',
-    Referer: 'https://epnex.io/phoneSelf_sign.html?i=00VHmxY&lan=0',
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
-  },
-  proxy: 'http://chosan.cn:12345'
-})
+// let r = rp.post('https://epnex.io/api/emailValidCode', {
+//   form: `{"user_email":"12314@chosan.cn","PvilidCode":"s7b4r"}`,
+//   headers: {
+//     Host: 'epnex.io',
+//     Origin: 'https://epnex.io',
+//     Referer: 'https://epnex.io/phoneSelf_sign.html?i=00VHmxY&lan=0',
+//     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
+//   },
+//   proxy: 'http://chosan.cn:12345'
+// })
 
-r.on('data', (err, data) => {
-  console.log(err, data);
+// r.on('data', (err, data) => {
+//   console.log(err, data);
+// })
+
+http.request({
+
 })
