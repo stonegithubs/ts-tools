644207269012f8c334810dfa90feeb5623798ea9
chosan <179817004@qq.com>
2018-07-05 15:19:03 +0800

    ..

--- src/lib/mail/utils.ts
+++ src/lib/mail/utils.ts
@@ -2,7 +2,7 @@ import { getDefaultChars, getRandomInt, getRandomStr } from "../utils";
 
 const { random, floor } = Math;
 
-export function gMail(suffix: (string | string[]) = ['@chosan.cn', '@ysd.kim', '@mln.fun', '@mlo.fun', '@mln.kim'], allowChars?: string[]): string {
+export function gMail(suffix: (string | string[]) = ['@ysd.kim', '@mln.fun', '@mlo.fun', '@mln.kim'], allowChars?: string[]): string {
   suffix = Array.isArray(suffix) ? suffix[getRandomInt(suffix.length)] : suffix;
   return getRandomStr(15, 5, allowChars) + suffix;
 }
