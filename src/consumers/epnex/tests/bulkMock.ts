e18f261b9859eea1d49df6ca37bae828fd1ed6a4
Chosan <179817004@qq.com>
2018-07-07 10:56:27 +0800

    ..

--- src/consumers/epnex/tests/bulkMock.ts
+++ src/consumers/epnex/tests/bulkMock.ts
@@ -6,10 +6,14 @@ let mongo = new Mongo();
 
 mongo.getCollection('epnex', 'regists').then(col => {
     let cur = col.find();
-    cur.forEach(item => {
+    let count = 0;
+    cur.forEach((item) => {
+        count++;
+        log(`当前第\t${count}\t条数据`);
         let ep = new Epnex(item.invitation);  // '00TPBBT'
-        let randTime = getRandomInt(3600 * 10);
+        let randTime = getRandomInt(3600);
         log(`将在\t${randTime}\t秒钟之后模拟用户操作！`);
+
         setTimeout(async () => {
             await ep.login(item.user_email, item.user_password);
             ep.mockOperation();
