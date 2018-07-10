864d6dd55f3d883350069b4cd518e31b0fb3057d
chosan <179817004@qq.com>
2018-07-10 14:50:08 +0800

    更换目录结构

--- fe/app/pods/components/some-input/component.ts
+++ fe/app/pods/components/some-input/component.ts
@@ -2,6 +2,12 @@ import Component from '@ember/component';
 
 export default class SomeInput extends Component.extend({
   // anything which *must* be merged to prototype here
+  // init() {
+  //   this._super(...arguments);
+  // }
 }) {
   // normal class body definition here
+  constructor(){
+    super(...arguments);
+  }
 };
