e5dc20a81f7749e5203470f80ad715010336378b
chosan <179817004@qq.com>
2018-07-18 17:55:48 +0800

    ..

--- ts-lib/src/consumers/survey/index.ts
+++ ts-lib/src/consumers/survey/index.ts
@@ -1,7 +1,7 @@
 import Koa from '../../lib/koa';
 import Mongo from '../../lib/mongo/';
 import reverseConf from '../../conf/reverseProxyConf';
-import { log } from 'util';
+import { log } from '../../lib/utils';
 
 let mongo = new Mongo();
 
@@ -11,9 +11,11 @@ new Koa([
     path: '/serveies',
     cb: async ctx => {
       let survey = ctx.request.body;
+      log('数据接收到!');
       let col = await mongo.getCollection('survey', 'survey')
       let result = await col.insertOne(survey);
       ctx.body = result.result;
+      log('数据写入完成!', result);
     }
   }
 ]).listen(reverseConf.survey.port);
