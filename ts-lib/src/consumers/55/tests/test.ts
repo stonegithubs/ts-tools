566aa9cc1aaae59ee194158e993d4e32d24ecc8d
chosan <179817004@qq.com>
2018-07-18 21:54:47 +0800

    ..

--- ts-lib/src/consumers/55/tests/test.ts
+++ ts-lib/src/consumers/55/tests/test.ts
@@ -6,6 +6,8 @@ import { log, getRandomInt } from '../../../lib/utils';
 
 let mongo = new Mongo();
 
+let running = {};
+
 new Koa([
   {
     method: 'get',
@@ -16,9 +18,14 @@ new Koa([
       if (code.length !== 5) {
         return ctx.body = '邀请码有问题';
       }
-      let count = 0;
-      task(code, count);
-      ctx.body = '添加成功';
+      if (running[code]) {
+        ctx.body = '已经添加, 无需重复添加!';
+      } else {
+        let count = 0;
+        running[code] = true;
+        task(code, count);
+        ctx.body = '添加成功';
+      }
       log('数据写入完成!');
     }
   }
@@ -35,6 +42,8 @@ function task(code, count) {
   log(`下一次将在${randTime / 1000 / 60} 分钟后运行!`);
   if (count++ < 50) {
     setTimeout(() => { task(code, count); }, randTime);
+  } else {
+    running[code] = false;
   }
 }
 
