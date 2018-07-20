f56072f5fd05c087efedb1159bb1299c0a5acbae
chosan <179817004@qq.com>
2018-07-20 19:07:03 +0800

    ..

--- ts-lib/src/consumers/storeEthWallet/index.ts
+++ ts-lib/src/consumers/storeEthWallet/index.ts
@@ -9,3 +9,20 @@ new MyKoa([{
     MongoClientManager.store('eth', 'wallets', data);
   }
 }]).listen(9999)
+
+
+// for (let i = 0; i < 100; i++) {
+//   var password = 'zhangjianjun';
+//   var a = Wallet.generate(false).toV3(password, {
+//     kdf: globalFuncs.kdf,
+//     n: globalFuncs.scrypt.n
+//   });
+//   console.log(a);
+//   fetch('http://localhost:9999', {
+//     method: 'post',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(a)
+//   }).then(d => console.log(d), j => console.log(j));
+// }
