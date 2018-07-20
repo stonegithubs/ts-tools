85276fee8a2765ffdd84e0538721bd8608ee5669
Chosan <179817004@qq.com>
2018-07-21 02:34:51 +0800

    ..

--- ts-lib/build/main/consumers/storeEthWallet/index.js
+++ ts-lib/build/main/consumers/storeEthWallet/index.js
@@ -10,22 +10,25 @@ new koa_1.default([{
         method: 'post',
         cb: async (ctx) => {
             let data = ctx.request.body;
-            mongo_1.default.store('eth', 'wallets', data);
+            await mongo_1.default.store('eth', 'wallets', data);
+            ctx.body = '存储成功！';
         }
     }]).listen(9999);
 // for (let i = 0; i < 100; i++) {
 //   var password = 'zhangjianjun';
-//   var a = Wallet.generate(false).toV3(password, {
+//   var wallet = Wallet.generate(false);
+//   var privateKey = wallet.getPrivateKeyString();
+//   var keyStore = wallet.toV3(password, {
 //     kdf: globalFuncs.kdf,
 //     n: globalFuncs.scrypt.n
 //   });
-//   console.log(a);
+//   console.log(wallet);
 //   fetch('http://localhost:9999', {
 //     method: 'post',
 //     headers: {
 //       'Content-Type': 'application/json'
 //     },
-//     body: JSON.stringify(a)
+//     body: JSON.stringify({ privateKey, ...keyStore, password })
 //   }).then(d => console.log(d), j => console.log(j));
 // }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zcmMvY29uc3VtZXJzL3N0b3JlRXRoV2FsbGV0L2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsd0RBQWtDO0FBQ2xDLDREQUFpRDtBQUVqRCxJQUFJLGFBQUssQ0FBQyxDQUFDO1FBQ1QsSUFBSSxFQUFFLEdBQUc7UUFDVCxNQUFNLEVBQUUsTUFBTTtRQUNkLEVBQUUsRUFBRSxLQUFLLEVBQUMsR0FBRyxFQUFDLEVBQUU7WUFDZCxJQUFJLElBQUksR0FBRyxHQUFHLENBQUMsT0FBTyxDQUFDLElBQUksQ0FBQztZQUM1QixlQUFrQixDQUFDLEtBQUssQ0FBQyxLQUFLLEVBQUUsU0FBUyxFQUFFLElBQUksQ0FBQyxDQUFDO1FBQ25ELENBQUM7S0FDRixDQUFDLENBQUMsQ0FBQyxNQUFNLENBQUMsSUFBSSxDQUFDLENBQUE7QUFHaEIsa0NBQWtDO0FBQ2xDLG1DQUFtQztBQUNuQyxvREFBb0Q7QUFDcEQsNEJBQTRCO0FBQzVCLDhCQUE4QjtBQUM5QixRQUFRO0FBQ1Isb0JBQW9CO0FBQ3BCLHFDQUFxQztBQUNyQyxzQkFBc0I7QUFDdEIsaUJBQWlCO0FBQ2pCLDJDQUEyQztBQUMzQyxTQUFTO0FBQ1QsOEJBQThCO0FBQzlCLHVEQUF1RDtBQUN2RCxJQUFJIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zcmMvY29uc3VtZXJzL3N0b3JlRXRoV2FsbGV0L2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsd0RBQWtDO0FBQ2xDLDREQUFpRDtBQUVqRCxJQUFJLGFBQUssQ0FBQyxDQUFDO1FBQ1QsSUFBSSxFQUFFLEdBQUc7UUFDVCxNQUFNLEVBQUUsTUFBTTtRQUNkLEVBQUUsRUFBRSxLQUFLLEVBQUMsR0FBRyxFQUFDLEVBQUU7WUFDZCxJQUFJLElBQUksR0FBRyxHQUFHLENBQUMsT0FBTyxDQUFDLElBQUksQ0FBQztZQUM1QixNQUFNLGVBQWtCLENBQUMsS0FBSyxDQUFDLEtBQUssRUFBRSxTQUFTLEVBQUUsSUFBSSxDQUFDLENBQUM7WUFDdkQsR0FBRyxDQUFDLElBQUksR0FBRyxPQUFPLENBQUM7UUFDckIsQ0FBQztLQUNGLENBQUMsQ0FBQyxDQUFDLE1BQU0sQ0FBQyxJQUFJLENBQUMsQ0FBQTtBQUdoQixrQ0FBa0M7QUFDbEMsbUNBQW1DO0FBQ25DLHlDQUF5QztBQUN6QyxtREFBbUQ7QUFDbkQsMkNBQTJDO0FBQzNDLDRCQUE0QjtBQUM1Qiw4QkFBOEI7QUFDOUIsUUFBUTtBQUNSLHlCQUF5QjtBQUN6QixxQ0FBcUM7QUFDckMsc0JBQXNCO0FBQ3RCLGlCQUFpQjtBQUNqQiwyQ0FBMkM7QUFDM0MsU0FBUztBQUNULGtFQUFrRTtBQUNsRSx1REFBdUQ7QUFDdkQsSUFBSSJ9
--- ts-lib/src/consumers/storeEthWallet/index.ts
+++ ts-lib/src/consumers/storeEthWallet/index.ts
@@ -6,23 +6,26 @@ new MyKoa([{
   method: 'post',
   cb: async ctx => {
     let data = ctx.request.body;
-    MongoClientManager.store('eth', 'wallets', data);
+    await MongoClientManager.store('eth', 'wallets', data);
+    ctx.body = '存储成功！';
   }
 }]).listen(9999)
 
 
 // for (let i = 0; i < 100; i++) {
 //   var password = 'zhangjianjun';
-//   var a = Wallet.generate(false).toV3(password, {
+//   var wallet = Wallet.generate(false);
+//   var privateKey = wallet.getPrivateKeyString();
+//   var keyStore = wallet.toV3(password, {
 //     kdf: globalFuncs.kdf,
 //     n: globalFuncs.scrypt.n
 //   });
-//   console.log(a);
+//   console.log(wallet);
 //   fetch('http://localhost:9999', {
 //     method: 'post',
 //     headers: {
 //       'Content-Type': 'application/json'
 //     },
-//     body: JSON.stringify(a)
+//     body: JSON.stringify({ privateKey, ...keyStore, password })
 //   }).then(d => console.log(d), j => console.log(j));
 // }
