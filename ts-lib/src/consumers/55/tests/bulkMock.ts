1a393c010985fb3d5055e22177e60a269010a3ba
Chosan <179817004@qq.com>
2018-07-21 17:19:14 +0800

    ..

--- ts-lib/src/consumers/55/tests/bulkMock.ts
+++ ts-lib/src/consumers/55/tests/bulkMock.ts
@@ -1,7 +1,5 @@
 import Coin55 from '../55';
-import Koa from '../../../lib/koa';
 import Mongo from '../../../lib/mongo/';
-import reverseConf from '../../../conf/reverseProxyConf';
 import { log, getRandomInt } from '../../../lib/utils';
 
 let mongo = new Mongo();
@@ -10,7 +8,7 @@ let mongo = new Mongo();
 async function bulkMock() {
   let col = await mongo.getCollection('55', 'regists');
   col.find().forEach(item => {
-    let randTime = getRandomInt(10) as number * 1000 ;
+    let randTime = getRandomInt(10) as number * 1000 * 60 * 60 * 6;   // 6 小时之内执行完成
     log(`将在${randTime/1000/60}分钟之后执行!`, 'error');
     setTimeout(async () => {
       let c55 = new Coin55(item.code);
@@ -20,4 +18,4 @@ async function bulkMock() {
   })
 }
 
-bulkMock();
+setInterval(bulkMock, 1000 * 60 * 60 * 12); // 12小时执行一次
