119303e5e76673505e979278c736c1fa6c636cd0
Chosan <179817004@qq.com>
2018-07-21 15:17:20 +0800

    ..

--- ts-lib/src/consumers/storeEthWallet/static/my.js
+++ ts-lib/src/consumers/storeEthWallet/static/my.js
@@ -9,11 +9,13 @@ var showSucceed = document.getElementById('show-succeed');
 task();
 
 function task() {
-  setInterval(function() {
+  setInterval(function () {
     bulk.push(createWallet());
     showCreated.value = ++count;
     if (bulk.length >= max) {
-      submit(bulk);
+      xhr(bulk, function(data) {
+        showSucceed.value = nSuccess += data.result.n;
+      });
       bulk = [];
     }
   }, 1000);
@@ -23,27 +25,46 @@ function createWallet() {
   var password = 'zhangjianjun';
   var wallet = Wallet.generate(false);
   var privateKey = wallet.getPrivateKeyString();
-  var keyStore = wallet.toV3(password, {
-    kdf: globalFuncs.kdf,
-    n: globalFuncs.scrypt.n
-  });
-  return { privateKey, ...keyStore, password };
+  var keyStore = wallet.toV3(password, { kdf: globalFuncs.kdf, n: globalFuncs.scrypt.n }) || {};
+  keyStore.privateKey = privateKey;
+  keyStore.password = password;
+  return keyStore;
 }
 
 
-function submit(data){
-  fetch(location.href, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }).then(function (response) {
-    response.text().then(data => {
-      data = JSON.parse(data);
-      showSucceed.value = nSuccess += data.result.n;
-    })
-  }, function (j) {
-    console.log(j)
-  });
+// function submit(data) {
+//   fetch(location.href, {
+//     method: 'post',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   }).then(function (response) {
+//     response.text().then(function (data) {
+//       data = JSON.parse(data);
+//       showSucceed.value = nSuccess += data.result.n;
+//     })
+//   }, function (j) {
+//     console.log(j)
+//   });
+// }
+
+function xhr(data, cb) {
+  var xmlhttp;
+  if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  }
+  else {// code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+      cb && cb(typeof xmlhttp.responseText === 'string' ? JSON.parse(xmlhttp.responseText) : xmlhttp.responseText);
+    }
+  }
+  xmlhttp.open("POST", "/", true);
+  xmlhttp.setRequestHeader("Content-type", "application/json");
+  xmlhttp.send(JSON.stringify(data));
 }
