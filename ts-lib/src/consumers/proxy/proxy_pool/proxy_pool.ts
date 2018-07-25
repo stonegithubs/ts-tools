7f0cde18ca242e4fbe6fec63b0aaccd6e7e58b12
chosan <179817004@qq.com>
2018-07-25 13:57:31 +0800

    ..

--- ts-lib/src/consumers/proxy/proxy_pool/proxy_pool.ts
+++ ts-lib/src/consumers/proxy/proxy_pool/proxy_pool.ts
@@ -47,7 +47,7 @@ export default class ProxyPoll{
         if (data.origin) {
           // OK
           log('checker 成功', data);
-          col.updateOne(el, {$set: { lastCheckTime: new Date().toString() }});
+          col.updateOne(el, {$set: { lastCheckTime: new Date().toLocaleString() }});
           count++;
         } else {
           log('checker 失败', data, 'warn');
