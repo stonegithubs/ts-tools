b7268616d4c0fcc16b08945147867debbd6bf0e8
chosan <179817004@qq.com>
2018-07-25 15:08:04 +0800

    ..

--- ts-lib/src/consumers/proxy/proxy_pool/proxy_pool.ts
+++ ts-lib/src/consumers/proxy/proxy_pool/proxy_pool.ts
@@ -29,7 +29,7 @@ export default class ProxyPoll{
       if (queue.length >= chekcParallelCount || !await cursor.hasNext()) {
         log(`从${count}条开始检测\t`, 'warn');
         let success = await this.doCheck(queue);
-        log(`检测成功, 共\t${queue.length}\t条, 成功\t${success}\t条!`, 'warn');
+        log(`检测成功, 参与检测\t${queue.length}\t条, 成功\t${success}\t条!`, 'warn');
         count += queue.length;
         queue = [];
       }
