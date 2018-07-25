b5a21c0f9bacb6709f6638a4aae68e914884e0b5
chosan <179817004@qq.com>
2018-07-25 10:38:43 +0800

    ..

--- ts-lib/src/consumers/proxy/proxy_pool/tests/test.ts
+++ ts-lib/src/consumers/proxy/proxy_pool/tests/test.ts
@@ -17,7 +17,7 @@ new Koa([
     cb: async ctx => {
       let { count = 100, begin = 0 } = ctx.query;
       let col = await mongo.getCollection(dbName, colName);
-      let data = await col.find().limit(count).skip(begin);
+      let data = await col.find().limit(count).skip(begin).toArray();
       ctx.body = {
         status: 1,
         data
