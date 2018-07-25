3aa19880ee83dcd3fb1ebc34bc34413cfee609b5
chosan <179817004@qq.com>
2018-07-25 20:20:23 +0800

    ..

--- ts-lib/src/consumers/proxy/proxy_pool/proxy_pool.ts
+++ ts-lib/src/consumers/proxy/proxy_pool/proxy_pool.ts
@@ -67,16 +67,15 @@ export default class ProxyPoll{
         let data = await MyReq.getJson('http://httpbin.org/ip', {}, 'get', {
           rejectUnauthorized: false, agent,
           headers: {
-            Connection: 'keep-alive',
-            Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Pragma': 'no-cache',
+            'Host': 'httpbin.org',
+            'User-Agent': randomUA(),
+            'Cache-Control': 'no-cache',
+            'Upgrade-Insecure-Requests': 1,
+            'Proxy-Connection': 'keep-alive',
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'Cache-Control': 'no-cache',
-            Host: 'httpbin.org',
-            Pragma: 'no-cache',
-            'Proxy-Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': 1,
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
           }
         });
         if (data.origin) {
