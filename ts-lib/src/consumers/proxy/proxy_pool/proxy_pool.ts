242b4bfee70f01bc1099c363d7b175081d30248f
chosan <179817004@qq.com>
2018-07-26 13:32:19 +0800

    ..

--- ts-lib/src/consumers/proxy/proxy_pool/proxy_pool.ts
+++ ts-lib/src/consumers/proxy/proxy_pool/proxy_pool.ts
@@ -44,7 +44,7 @@ export default class ProxyPool{
   }
   async checker() {
     let col = await mongo.getCollection('proxy', 'proxys');
-    let cursor = await col.find().noCursorTimeout();  // noCursorTimeout() 防止游标超时导致被数据库释放, 需要主动关闭游标
+    let cursor = await col.find().addCursorFlag('noCursorTimeout', true);  // noCursorTimeout 防止游标超时导致被数据库释放, 需要主动关闭游标
     let chekcParallelCount = 400; // 一次检测400条
     let count = 0;
     let queue = [];
