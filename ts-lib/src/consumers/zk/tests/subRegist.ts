0539dad94bde20111efdef2a1f947f01883ec4cc
chosan <179817004@qq.com>
2018-07-31 17:18:40 +0800

    ..

--- ts-lib/src/consumers/zk/tests/subRegist.ts
+++ ts-lib/src/consumers/zk/tests/subRegist.ts
@@ -1,10 +1,11 @@
 import Mongo from '../../../lib/mongo/';
 import ZK from '../zk';
+import { getRandomInt } from '../../../lib/utils';
 
 
 new Mongo().getCollection('zk', 'regists').then(col => {
   let count = 1;
-  col.find({txtCode: '3E3C23A511B835C'}).limit(30).forEach(el => {
+  col.find({txtCode: '3E3C23A511B835C'}).limit(getRandomInt(20, 5)).forEach(el => {
     let zk = new ZK(el.txtCode);
     zk.task(count++);
   })
